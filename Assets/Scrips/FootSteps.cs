22380a10b611e33677e4bac635479318c1dcf924
CurZedd <Ludvig.strauss@elev.ga.lbs.se>
2024-01-15 12:16:07 +0100

    sprinting footsteps

--- Assets/Scrips/FootSteps.cs
+++ Assets/Scrips/FootSteps.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
-    public AudioSource footstepssound;
+    public AudioSource footstepssound, sprintsound;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.D)))
         {
             footstepssound.enabled = true;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                footstepssound.enabled = false;
+                sprintsound.enabled = true;
+            }
+            else
+            {
+                footstepssound.enabled = true;
+                sprintsound.enabled = false;
+            }
         }
         else
         {
             footstepssound.enabled = false;
+            sprintsound.enabled = false;
         }
     }
 }
