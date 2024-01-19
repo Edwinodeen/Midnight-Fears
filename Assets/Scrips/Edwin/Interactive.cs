a7be0c42b0c931ba11209b8606a2175bc9c1fb47
Edwinodeen <edwin.odeen@gmail.com>
2024-01-19 10:06:15 +0100

    OPENTHEDOOOOR

---
+++ Assets/Scrips/Edwin/Interactive.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Interactive Object Interaction in C#");
+
+        // Interactive menu
+        while (true)
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1. Create a new person");
+            Console.WriteLine("2. Display information about a person");
+            Console.WriteLine("3. Exit");
+
+            Console.Write("Enter your choice (1-3): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CreatePerson();
+                    break;
+                case "2":
+                    DisplayPersonInfo();
+                    break;
+                case "3":
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
+                    break;
+            }
+        }
+    }
+
+    static void CreatePerson()
+    {
+        Console.Write("Enter the person's name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter the person's age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Person newPerson = new Person { Name = name, Age = age };
+        Console.WriteLine("Person created successfully!");
+    }
+
+    static void DisplayPersonInfo()
+    {
+        // Assuming we have a globally accessible Person object (for simplicity)
+        if (newPerson != null)
+        {
+            newPerson.DisplayInfo();
+        }
+        else
+        {
+            Console.WriteLine("No person created yet. Please create a person first.");
+        }
+    }
+
+    // Global variable to store the created Person object
+    static Person newPerson;
+}
+
---
+++ Assets/Scrips/Edwin/PlayerInteraction.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public Camera mainCam;
+
+    public float interactionDistance = 2f;
+
+    public GameObject interactionUI;
+    public TextMeshProUGUI interactionText;
+    public LayerMask mask;
+
+    private void Update()
+    {
+        InteractionRay();
+    }
+
+    void InteractionRay()
+    {
+        Ray ray = mainCam.ViewportPointToRay(Vector3.one / 2f);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * interactionDistance);
+        bool hitsomething = false;
+
+        if (Physics.Raycast(ray, out hit, interactionDistance, mask))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                hitsomething = true;
+                interactionText.text = interactable.GetDescription();
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+
+        interactionUI.SetActive(hitsomething);
+
+    }
+
+}
